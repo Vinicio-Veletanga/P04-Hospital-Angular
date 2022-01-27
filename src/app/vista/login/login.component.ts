d95e48f8102f679065872af343d6b455a76ab186
dmauricio4 <diegomarshall4@gmail.com>
2022-01-26 23:42:52 -0500

    Dashboard

--- src/app/vista/login/login.component.ts
+++ src/app/vista/login/login.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { ApiService } from '../../services/api/api.service';
 import { LoginI } from '../../modelos/login.interface';
+import { ResponseI } from 'src/app/modelos/response.interface';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -12,19 +14,29 @@ import { LoginI } from '../../modelos/login.interface';
 export class LoginComponent implements OnInit {
 
   loginForm = new FormGroup({
-    usuario : new FormControl('', Validators.required),
+    correo : new FormControl('', Validators.required),
     password : new FormControl ('', Validators.required)
   })
 
-  constructor(private api:ApiService) { }
+  constructor(private api:ApiService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  
   onLogin(form:LoginI){
+    /*
     this.api.loginByEmail(form).subscribe(data => {
-      console.log(data)
+      console.log(form)
+      let dataResponse:ResponseI = data;
+      if(dataResponse.status == "ok"){
+        localStorage.setItem("token",dataResponse.result.token);
+        this.router.navigate(['dashboard']);
+      }
+      
     });
+    */
+    
   }
 
 }
